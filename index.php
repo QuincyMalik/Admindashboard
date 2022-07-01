9d67849cda3f5a7f5d0be28f5093db68c41d20c4
donclique <donclique3@gmail.com>
2022-07-01 13:58:33 +0300

    FEAT added student module

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
 	<nav>
 	  <ul>
 		<li>
-			<a href="">
+			<a href="students.html">
 				<span> <i class="fa fa-group"></i></span>
 				<span>Students</span>
 			</a>
@@ -42,7 +42,7 @@
 	</nav>
 </div>
 <div class="Main-content">
-	 <div class="container-fluid">
+ <div class="container-fluid">
 	  <div class="row">
 	   <div class="col-lg-12">
 	     <div class="card-header bg-dark text-white text-center">
@@ -50,14 +50,14 @@
 				</div>
 			</div>
 		</div>
-	</div>
-<div class="row">
+	 </div>
+  <div class="row" style="padding-left:15px;">
 	<div class="col-lg-3">
 		<div class="card-header bg-dark text-white text-center">
 			<span>Students</span>
 		</div>
 		<div class="card-body"></div>
-		<span>><i class="fa fa-group fa-3x"></i>></span>
+		<span><i class="fa fa-group fa-3x"></i></span>
 		<span class="float-right">00</span>
 		<div class="card-footer"></div>
 	</div>
@@ -66,7 +66,7 @@
 			<span>Available Courses</span>
 		</div>
 		<div class="card-body"></div>
-		<span>><i class="fa fa-folder fa-3x"></i>></span>
+		<span><i class="fa fa-folder fa-3x"></i></span>
 		<span class="float-right">00</span>
 		<div class="card-footer"></div>
 	</div>
@@ -75,28 +75,35 @@
 			<span>Campuses</span>
 		</div>
 		<div class="card-body"></div>
-		<span>><i class="fa fa-graduation-cap fa-3x"></i>></span>
+		<span><i class="fa fa-graduation-cap fa-3x"></i></span>
 		<span class="float-right">00</span>
 		<div class="card-footer"></div>
 	</div>
-	<div class="col-lg-3">
+	<div class="col-lg-3" style="padding-right:30px;">
 		<div class="card-header bg-dark text-white text-center">
-			<span>Top content</span>
+			<span>Courses</span>
 		</div>
 		<div class="card-body"></div>
-		<span>><i class="fa fa-user fa-3x"></i>></span>
+		<span><i class="fa fa-user fa-3x"></i></span>
 		<span class="float-right">00</span>
 		<div class="card-footer"></div>
 	</div>
+  </div>
+	<div class="row">
 	<div class="col-lg-12">
 		<div class="card-header bg-dark text-white text-center">
 			<span>Student Analysis</span>
 		</div>
-	</div>	
-</div>
+  <div class="body-shadow">
+	<span><i class="fa fa-line-chart fa-4x"></i></span>
+			</div>
+		</div>	
+	</div>
+  </div>
 </div>
 	
 <script src="jquery.min.js"></script>
+<script src="bootstrap-5.2.0/js/bootstrap.bundle.min.js"></script>
 <script src="bootstrap/js/bootstrap.min.js"></script>
 </body>
 </html>
